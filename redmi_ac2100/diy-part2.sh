cf06cc49c90c684219a100fbfe72db85ea7b0332
lZackx <lzackx@lzackx.com>
2021-07-11 21:32:32 +0800

    Update diy-part2.sh

--- redmi_ac2100/diy-part2.sh
+++ redmi_ac2100/diy-part2.sh
@@ -35,7 +35,7 @@ rm -rf package/lean/luci-theme-ifit
 #git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/lean/luci-theme-infinityfreedom
 #git clone https://github.com/XXKDB/luci-theme-argon_armygreen.git package/lean/luci-theme-argon_armygreen
 #git clone https://github.com/YL2209/luci-theme-ifit.git package/lean/luci-theme-ifit
-cp -rf ../luci-theme-argon_armygreen package/lean/luci-theme-argon_armygreen
+cp -rf ../../luci-theme-argon_armygreen package/lean/luci-theme-argon_armygreen
 
 # 修改luci-theme-argon_armygreen主题渐变色，16进制RGB
 #登录页面背景颜色 透明值
